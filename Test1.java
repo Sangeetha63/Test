f90cd8a1f78ae3610af06cbce2c505fc10c51091
Sangeetha63 <113541924+Sangeetha63@users.noreply.github.com>
2023-01-06 13:07:56 +0530

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,92 @@
+1.class TwoFunction{
+public static void main(String args[]){
+ Scanner sc=new Scanner(System.in);
+ Arrays.sort(a);  
+return a[total-1]; 
+ 
+		 
+}
+}
+
+2.class EvenOddPetals{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+           int x;
+		
+		System.out.println("enter an flowers");
+		x=sc.nextInt();	
+		if(x%10==0) {
+			System.out.println("even number of petals");
+		}
+		else {
+			
+			System.out.println("odd number of petals");
+		}
+	}
+
+}
+
+3.* generalised is a class and parent class
+  *specialized is  class and child class
+
+4.a) not use extend keyword
+b)  class program{
+	private String name;
+	
+}
+c)delegation is alternatives to inheritance
+
+5.*CompareTo
+  *Comparable
+
+6.      Arraylist:
+* its is internally use array
+* it is faster in read operation
+* it is slower in write operation                           
+
+      LinkedList:
+* its is internally use nodes
+* it is slower in read operation
+* it is  faster in write operation 
+
+
+7.     iterator:
+* its is forward
+*it is add method
+
+      listIterator:
+*it is a forward and backward 
+* its is both add and remove method
+
+
+8.Covariant method in overriding in java
+= it is child class and return type is vary.
+
+
+9.memory leakages in java
+=object present in heap memory and it removed by Garbage  collector.
+
+
+10. static plymorphism:
+     *function call with obiject in complile time
+=    * it is called method overloading
+     * different return type
+
+
+
+   Dynamic polymorphism:
+   *function call with obiject in runt time
+=  *it is called as method overriding
+   *same return type
+
+11.Encapsulation
+
+
+
+
+
+
+
+
+
+
